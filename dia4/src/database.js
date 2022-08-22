593d02614691da537bc0de0ad6981e082e2b993a
Daniel <danielvazquezheredia1994@gmail.com>
2022-08-22 12:18:21 +0200

    fin proyecto

--- dia4/src/database.js
+++ dia4/src/database.js
@@ -3,11 +3,7 @@ const mysql = require('mysql2')
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-<<<<<<< HEAD
     password: 'alohomora',
-=======
-    password: '--password-here--', //! PONER PASSWORD
->>>>>>> 0b54bd178414beb78f7677fd005480490a272cc0
     database: 'school'
 });
 
